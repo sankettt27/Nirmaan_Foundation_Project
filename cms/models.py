907fada0274c44ee6a7e5f820c0b2971969b5276
sankettt27 <sanketzinjurke83@gmail.com>
2026-09-25 14:20:50 +0530

    Fix video embed URL logic for Media CMS

--- cms/models.py
+++ cms/models.py
@@ -804,3 +804,14 @@ class Video(models.Model):
 
     def __str__(self):
         return self.description or f"Video {self.id}"
+
+    @property
+    def embed_url(self):
+        """Convert standard YouTube URLs to embed URLs for iframes."""
+        if 'youtube.com/watch?v=' in self.video_url:
+            video_id = self.video_url.split('v=')[1][:11]
+            return f"https://www.youtube.com/embed/{video_id}"
+        elif 'youtu.be/' in self.video_url:
+            video_id = self.video_url.split('youtu.be/')[1][:11]
+            return f"https://www.youtube.com/embed/{video_id}"
+        return self.video_url
